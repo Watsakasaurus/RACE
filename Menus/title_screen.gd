c5fab5106bf4d77589df718e6f06b50b35fe3df5
Watsakasaurus <cazwatson@dundee.ac.uk>
2021-03-05 17:13:55 +0000

    menu system changes, select screen progressed

--- Menus/title_screen.gd
+++ Menus/title_screen.gd
@@ -3,7 +3,13 @@ extends Control
 var scene_path_to_load
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for button in $Menu/CentreRow/MenuButtons.get_children():
+	for button in $Menu/CentreRow/RightColumn/NewGameBox.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+	for button in $Menu/CentreRow/RightColumn/ExitBox.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+	for button in $Menu/CentreRow/LeftColumn/PlayBar.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
 func _on_Button_pressed(scene_to_load):
